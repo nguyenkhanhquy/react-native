8c0af50ccd8344154c781b2ca921db8016111cc3
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-10 01:25:10 +0700

    setup: Tailwind CSS

--- 21110282_NguyenKhanhQuy_BaitapA03/tailwind.config.js
+++ 21110282_NguyenKhanhQuy_BaitapA03/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
