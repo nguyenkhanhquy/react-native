1bf573a648c0379f0b157738a4e60eb37d95f611
Nguyễn Khánh Quy <120696079+nguyenkhanhquy@users.noreply.github.com>
2024-09-23 01:16:37 +0700

    refactor: API service

--- 21110282_NguyenKhanhQuy/app/services/AuthAPIService.js
+++ 21110282_NguyenKhanhQuy/app/services/AuthAPIService.js
@@ -1,20 +1,4 @@
-import AxiosClient from "../api/AxiosClient";
-
-const postRequest = async (url, data, headers) => {
-    try {
-        const response = await AxiosClient.post(url, data, headers);
-        return response.data;
-    } catch (error) {
-        // Xử lý lỗi và trả về dữ liệu lỗi nếu có
-        if (error.response) {
-            // Nếu có phản hồi lỗi từ server
-            return error.response.data; // Trả về dữ liệu lỗi từ server
-        } else {
-            // Nếu không có phản hồi lỗi từ server (lỗi mạng, v.v.)
-            throw error;
-        }
-    }
-};
+import { postRequest } from "../api/Request";
 
 export const login = async (email, password) => {
     const data = { email, password };
--- 21110282_NguyenKhanhQuy/app/services/JobAPIService.js
+++ 21110282_NguyenKhanhQuy/app/services/JobAPIService.js
@@ -1,20 +1,4 @@
-import AxiosClient from "../api/AxiosClient";
-
-const getRequest = async (url, headers) => {
-    try {
-        const response = await AxiosClient.get(url, headers);
-        return response.data;
-    } catch (error) {
-        // Xử lý lỗi và trả về dữ liệu lỗi nếu có
-        if (error.response) {
-            // Nếu có phản hồi lỗi từ server
-            return error.response.data; // Trả về dữ liệu lỗi từ server
-        } else {
-            // Nếu không có phản hồi lỗi từ server (lỗi mạng, v.v.)
-            throw error;
-        }
-    }
-};
+import { getRequest } from "../api/Request";
 
 export const getListJobs = async () => {
     return getRequest("jobs");
